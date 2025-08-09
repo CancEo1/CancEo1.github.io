230eea81dbd7c4eb311f827ec34caf102fd22c32
Cody Kern <143668697+CancEo1@users.noreply.github.com>
2025-08-08 21:37:39 -0600

    Update app.js
    Typewriter text effects

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,16 @@
 document.querySelector("button").addEventListener("click", () => {
   alert("Welcome to CancEo’s cosmic realm!");
 });
+
+const title = document.querySelector("h1");
+let i = 0;
+const text = "Welcome to CancEo Gaming...";
+function typeWriter() {
+  if (i < text.length) {
+    title.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 100);
+  }
+}
+typeWriter();
+
